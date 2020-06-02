92d7bef9a3b0ed9db1e39c98a4bff4288dc1f86d
vastlaan <vastlaan@mail.com>
2020-06-02 22:47:08 +0200

    logo og

--- client/src/components/seo.js
+++ client/src/components/seo.js
@@ -55,7 +55,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `image`,
-          content: "../images/logo.svg",
+          content: "/images/me2.png",
         },
         {
           name: `canonicalUrl`,
@@ -67,7 +67,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: "../images/logo.svg",
+          content: "/images/me2.png",
         },
         {
           property: `og:description`,
